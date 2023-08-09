b6d094cec1cf3cbdd2ba413948bca1f2b998fe73
jon-funk <jonathan.funk@runwhen.com>
2023-08-09 01:12:30 -0700

    Setup Package publish (#169)
    * Add package setup config
    
    * Add pypi action

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="runwhen-cli-keywords",
+    version=open("VERSION").read(),
+    packages=find_packages(),
+    license="Apache License 2.0",
+    description="A set of RunWhen published CLI keywords and python libraries for interacting with APIs using CLIs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Kyle Forster",
+    author_email="kyle.forster@runwhen.com",
+    url="https://github.com/runwhen-contrib/rw-cli-codecollection",
+    install_requires=required,
+    include_package_data=True,
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: Apache Software License"],
+)
