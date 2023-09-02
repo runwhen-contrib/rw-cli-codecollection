e3f2a87c44ca1cee98abecae3ca1a06fe550a961
Jonathan Funk <jonathan.funk@runwhen.com>
2023-09-02 00:27:47 +0000

    add object hint array multiplication

--- libraries/RW/NextSteps/Suggest.py
+++ libraries/RW/NextSteps/Suggest.py
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 
 ROBOT_LIBRARY_SCOPE = "GLOBAL"
 NO_RESULT_STRING: str = "No next steps found, contact your service owner."
+OBJECT_HINT_SYMBOL: str = ":"
 
 THIS_DIR: str = "/".join(__file__.split("/")[:-1])
 
@@ -29,11 +30,42 @@ def _load_mapping(platform: str) -> dict:
     return data
 
 
-def format(suggestions: str, **kwargs) -> str:
+def format(suggestions: str, expand_arrays: bool = True, **kwargs) -> str:
+    reformatted_suggestions: list[str] = []
+    suggestions = suggestions.split("\n")
+    formatted_kwargs: dict = {}
     for k, v in kwargs.items():
         if type(v) is str:
             kwargs[k] = v.strip("\n")
-    suggestions = Template(suggestions).safe_substitute(kwargs)
+        # create consistent key naming for dynamic hint types (lowercase all)
+        # eg: Pod_name -> pod_name
+        formatted_kwargs[k.lower()] = v
+    logger.info(f"Formatting with prepared kwargs: {formatted_kwargs}")
+    # for array values, multiplies them against matching object hint lines
+    # allowing nextsteps to point to multiple objects, like a labeled group of pods
+    if expand_arrays:
+        reformatted_kwargs: dict = {}
+        for line in suggestions:
+            for k, v in formatted_kwargs.items():
+                if k in line and OBJECT_HINT_SYMBOL in line and type(v) is list:
+                    line_parts = line.split(OBJECT_HINT_SYMBOL)
+                    if len(line_parts) != 2:
+                        logger.info(f"The object hint line is malformed: {line_parts}")
+                        continue
+                    object_type = line_parts[0]
+                    object_var_name = line_parts[1]
+                    new_lines: list[str] = []
+                    # eg: kwargs[pod_name] = ["myapp-0303", "myapp-3221"] -> kwargs[pod_name0] = "myapp-0303", etc
+                    for index, subval in enumerate(v):
+                        reformatted_kwargs[f"{k}{index}"] = subval
+                    new_lines = [f"{object_type}{index}" for index, subval in enumerate(v)]
+                    reformatted_suggestions += new_lines
+                else:
+                    reformatted_kwargs[k] = v
+                    reformatted_suggestions.append(line)
+        formatted_kwargs = reformatted_kwargs
+    suggestions = "\n".join(suggestions)
+    suggestions = Template(suggestions).safe_substitute(formatted_kwargs)
     return suggestions
 
 
@@ -72,9 +104,9 @@ def suggest(
         titles: list = []
         object_hints: list = []
         for suggestion in next_steps_data:
-            if ":" in suggestion and suggestion not in object_hints:
+            if OBJECT_HINT_SYMBOL in suggestion and suggestion not in object_hints:
                 object_hints.append(f"{suggestion}")
-            elif ":" not in suggestion and suggestion not in titles:
+            elif OBJECT_HINT_SYMBOL not in suggestion and suggestion not in titles:
                 titles.append(f"{suggestion}")
         object_hints = "\n".join(object_hints)
         titles = ", ".join(titles)
