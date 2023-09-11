07fe1747ecd28598622b89b2d90a12c04743e5cf
Shea Stewart <stewart.shea@gmail.com>
2023-09-11 12:07:27 +0000

    script cleanup

--- codebundles/k8s-deployment-healthcheck/deployment_logs.sh
+++ codebundles/k8s-deployment-healthcheck/deployment_logs.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-# Add script requirements
-# Need to get this passed through from runtime
+# -----------------------------------------------------------------------------
+# Script Information and Metadata
+# -----------------------------------------------------------------------------
+# Author: @stewartshea
+# Description: This script is designed to fetch and process Kubernetes logs 
+# and provide helpful insights based on the logs. It uses lnav to sift and 
+# query the logs for detail, and then tryies to match namespace resources with 
+# some of the text created by the queries. This can be extended as needed to 
+# cover many logfile use cases
+# -----------------------------------------------------------------------------
+
+# Update PATH to ensure script dependencies are found
 export PATH="$PATH:$HOME/.lnav:$HOME/.local/bin"
 
-# Ensure ${KUBERNETES_DISTRIBUTION_BINARY} is installed and accessible
-if ! command -v ${KUBERNETES_DISTRIBUTION_BINARY} &> /dev/null; then
-    echo "${KUBERNETES_DISTRIBUTION_BINARY} could not be found"
-    exit
-fi
+# -------------------------- Function Definitions -----------------------------
 
-# Ensure jq is installed and accessible
-if ! command -v jq &> /dev/null; then
-    echo "jq could not be found"
-    exit
-fi
+# Check if a command exists
+function check_command_exists() {
+    if ! command -v $1 &> /dev/null; then
+        echo "$1 could not be found"
+        exit
+    fi
+}
 
-# Ensure lnav is installed and accessible
-if ! command -v lnav &> /dev/null; then
-    echo "lnav could not be found"
-    exit
-else
-    # Load custom formats for lnav
-    cp -rf /collection/extras/lnav/formats/* $HOME/.lnav/formats/installed
-fi
+# ------------------------- Dependency Verification ---------------------------
+
+# Ensure all the required binaries are accessible
+check_command_exists ${KUBERNETES_DISTRIBUTION_BINARY}
+check_command_exists jq
+check_command_exists lnav
+
+# Load custom formats for lnav if it's installed
+# FIXME: This needs to sort out dev and runtime environment
+cp -rf /collection/extras/lnav/formats/* $HOME/.lnav/formats/installed
+cp -rf /workspace/codecollection-devtools/codecollection/extras/lnav/formats/* $HOME/.lnav/formats/installed
 
 
 # Ensure a deployment name was provided
@@ -32,13 +43,15 @@ if [ -z "$DEPLOYMENT_NAME" ]; then
     exit 1
 fi
 
+# Fetch label selectors for the provided deployment
 SELECTOR=$(${KUBERNETES_DISTRIBUTION_BINARY} get deployment $DEPLOYMENT_NAME --namespace=$NAMESPACE -o=jsonpath='{.spec.selector.matchLabels}' | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' | tr '\n' ',' | sed 's/,$//')
-
 if [ -z "$SELECTOR" ]; then
     echo "No label selectors found for Deployment $DEPLOYMENT_NAME."
     exit 1
 fi
 
+# Iterate through the pods based on the selector and fetch logs
+LOG_FILES=() 
 while read POD; do
     CONTAINERS=$(${KUBERNETES_DISTRIBUTION_BINARY} get pod $POD --namespace=$NAMESPACE -o=jsonpath='{range .spec.containers[*]}{.name}{"\n"}{end}')
     for CONTAINER in $CONTAINERS; do
@@ -56,44 +69,57 @@ while read POD; do
             LOGS=$(${KUBERNETES_DISTRIBUTION_BINARY} logs $POD -c $CONTAINER --namespace=$NAMESPACE  --limit-bytes=256000 --since=3h --context=${CONTEXT} -n ${NAMESPACE})
         fi
         
-        # Check if the first line of logs is in JSON format
+        # Check log format and store appropriately
         FIRST_LINE=$(echo "$LOGS" | head -n 1)
-        if echo "$FIRST_LINE" | jq -e . &>/dev/null; then
-            EXT="json"
-        else
-            EXT="txt"
-        fi
-        
-        # Output to a file
+        EXT=$(echo "$FIRST_LINE" | jq -e . &>/dev/null && echo "json" || echo "txt")
         FILENAME="${POD}_${CONTAINER}_logs.$EXT"
-        # Add file name to array
         LOG_FILES+=("$FILENAME")
         echo "Fetching logs for Pod: $POD, Container: $CONTAINER. Saving to $FILENAME."
         echo "$LOGS" > $FILENAME
     done
 done < <(${KUBERNETES_DISTRIBUTION_BINARY} get pods --selector=$SELECTOR --namespace=$NAMESPACE -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
 
-# Loop through each file and look for 
-# Work needs to be done here to scale this - as we have hard coded in the fields and the format - need to figure out how to best match the right formats, or can we just use logline
+# ------------------------------- lnav queries --------------------------------
+# The gist here is to provide various types of lnav queries. If a query has
+# results, then we can perform some additional tasks that suggest resources
+# which might be related
+#-------------------------------------------------------------------------------
+
+
+# NOTE: Work needs to be done here to scale this - as we have hard coded in the 
+# fields and the format - need to figure out how to best match the right formats, 
+# or can we just use logline
+
+##### Begin query #####
+# Search for http log format used by online-boutique
 for FILE in "${LOG_FILES[@]}"; do
     echo "$FILE"
-    # LOG_SUMMARY=$(lnav -n -c ';SELECT COUNT(*) AS error_count, CASE WHEN "http.req.path" LIKE "/product%" THEN "/product" ELSE "http.req.path" END AS root_path, "http.resp.status" FROM http_logrus_custom WHERE "http.resp.status" = 500 AND NOT "http.req.path" = "/" GROUP BY root_path, "http.resp.status" ORDER BY error_count DESC;' $FILE)
     LOG_SUMMARY=$(lnav -n -c ';SELECT COUNT(*) AS error_count, CASE WHEN "http.req.path" LIKE "/product%" THEN "/product" ELSE "http.req.path" END AS root_path, "http.resp.status" FROM logline WHERE "http.resp.status" = 500 AND NOT "http.req.path" = "/" GROUP BY root_path, "http.resp.status" ORDER BY error_count DESC;' $FILE)
     echo "$LOG_SUMMARY"
     INTERESTING_PATHS+=$(echo "$LOG_SUMMARY" | awk 'NR>1 && NR<5 {sub(/^\//, "", $2); print $2}')$'\n'
 done
 
-# Split out multiple paths and emove duplicate terms
-SEARCH_RESOURCES=$(echo "$INTERESTING_PATHS" | awk -F'/' '{for (i=1; i<=NF; i++) print $i}' | sort | uniq)
+if [[ -n "$INTERESTING_PATHS" ]]; then
+    SEARCH_RESOURCES=$(echo "$INTERESTING_PATHS" | awk -F'/' '{for (i=1; i<=NF; i++) print $i}' | sort | uniq)
+else
+    echo "No interesting paths found."
+fi
+##### End query #####
+
 
 # # Fetch a list of all resources in the namespace
 ## Heavyweight - this times out after 30s, but is a better way to get any and all resources
 # SEARCH_LIST=$(${KUBERNETES_DISTRIBUTION_BINARY} api-resources --verbs=list --namespaced -o name  | xargs -n 1 ${KUBERNETES_DISTRIBUTION_BINARY} get --show-kind --ignore-not-found -n $NAMESPACE)
 
 ## Lightweight - we explicitly specify which resources we want to interrogate
-RESOURCE_SEARCH_LIST=$(${KUBERNETES_DISTRIBUTION_BINARY}  get deployment,pods,service,statefulset --context=${CONTEXT} -n ${NAMESPACE})
+# Run RESOURCE_SEARCH_LIST only if SEARCH_RESOURCES has content
+if [[ -n "$SEARCH_RESOURCES" ]]; then
+    RESOURCE_SEARCH_LIST=$(${KUBERNETES_DISTRIBUTION_BINARY} get deployment,pods,service,statefulset --context=${CONTEXT} -n ${NAMESPACE})
+else
+    echo "No search queries returned results."
+    exit
+fi
 EVENT_SEARCH_LIST=$(${KUBERNETES_DISTRIBUTION_BINARY}  get events --context=${CONTEXT} -n ${NAMESPACE})
-
 event_details="The namespace ${NAMESPACE} has produced the following interesting events:"
 event_details+="\n"
 
@@ -104,6 +130,7 @@ for RESOURCE in "${SEARCH_RESOURCES[@]}"; do
     INTERESTING_RESOURCES+=$(echo "$RESOURCE_SEARCH_LIST" | grep "$RESOURCE")
 done
 
+# Try to generate some recommendations from the resource strings we discovered
 recommendations=()
 while read -r line; do
     # Splitting columns into array
@@ -138,9 +165,12 @@ while read -r line; do
     esac
 done <<< "$INTERESTING_RESOURCES"
 
+# Display the interesting events for report details
+printf "\nInteresting Events: \n"
 echo -e "$event_details"
 
+# Display all unique recommendations that can be shown as Next Steps
 if [[ ${#recommendations[@]} -ne 0 ]]; then
-    printf "Recommended Next Steps: \n"
+    printf "\nRecommended Next Steps: \n"
     printf "%s\n" "${recommendations[@]}" | sort -u
 fi
