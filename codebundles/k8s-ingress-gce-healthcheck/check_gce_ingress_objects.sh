631aba07697f30f8fb1fa4d93d86c7814efbe919
Shea Stewart <stewart.shea@gmail.com>
2023-10-30 01:47:57 +0000

    x

--- codebundles/k8s-ingress-gce-healthcheck/check_gce_ingress_objects.sh
+++ codebundles/k8s-ingress-gce-healthcheck/check_gce_ingress_objects.sh
@@ -29,25 +29,25 @@ BACKENDS=( $(echo $BACKENDS_JSON | jq -r 'keys[]') )  # Assuming jq is installed
 recommendations=()
 
 # Verify Forwarding Rule
-echo "=== Verifying Forwarding Rule $FORWARDING_RULE ==="
+echo "--- Verifying Forwarding Rule $FORWARDING_RULE ---"
 if ! gcloud compute forwarding-rules describe $FORWARDING_RULE --global --project=$GCP_PROJECT_ID &>/dev/null; then
     recommendations+=("Warning: Forwarding Rule [$FORWARDING_RULE] doesn't exist! Verify the correctness of the Ingress configuration and ensure the forwarding rule is properly created.")
 fi
 
 # Verify URL Map
-echo "=== Verifying URL Map $URL_MAP ==="
+echo "--- Verifying URL Map $URL_MAP ---"
 if ! gcloud compute url-maps describe $URL_MAP --global --project=$GCP_PROJECT_ID &>/dev/null; then
     recommendations+=("Warning: URL Map [$URL_MAP] doesn't exist! Check the associated ingress controller's logs and the GCP logs for any errors relating to the URL map creation.")
 fi
 
 # Verify Target Proxy (both HTTP and HTTPS)
-echo "=== Verifying Target Proxy $TARGET_PROXY ==="
+echo "--- Verifying Target Proxy $TARGET_PROXY ---"
 if ! gcloud compute target-https-proxies describe $TARGET_PROXY --global --project=$GCP_PROJECT_ID &>/dev/null && ! gcloud compute target-http-proxies describe $TARGET_PROXY --global --project=$GCP_PROJECT_ID &>/dev/null; then
     recommendations+=("Warning: Target Proxy [$TARGET_PROXY] doesn't exist! Ensure the Ingress is correctly set up to create the required target proxy.")
 fi
 
 # Display Backend Service's health status and check for problematic backends
-echo "=== Backend Service Health Status ==="
+echo "--- Backend Service Health Status ---"
 for backend in "${BACKENDS[@]}"; do
     health_status=$(gcloud compute backend-services get-health $backend --global --project=$GCP_PROJECT_ID)
     echo "Backend Service: $backend"
@@ -55,7 +55,7 @@ for backend in "${BACKENDS[@]}"; do
     echo "-----------------------------"
     
     if [[ ! $health_status =~ "HEALTHY" ]] || [[ $health_status =~ "UNHEALTHY" ]]; then
-        recommendations+=("Warning: Backend Service [$backend] has problematic health status. Check health checks and firewall rules for this backend.")
+        recommendations+=("Warning: Backend Service [$backend] has problematic health status. Check health checks and firewall rules for this backend. View GCP Logs. Verify IPs are on routable subnets (container-native load balancing) or using NodePort.")
     fi
 done
 
