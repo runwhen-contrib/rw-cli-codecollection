df783b772753df3e8cde9bff069337e3a7b3f5ce
Shea Stewart <stewart.shea@gmail.com>
2023-12-12 17:44:45 +0000

    wip

--- codebundles/k8s-deployment-gitops-gh-remediate/update_github_manifests.sh
+++ codebundles/k8s-deployment-gitops-gh-remediate/update_github_manifests.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+
+# Define labels or annotations for identifying gitops resources
+
+# Flux
+declare -A flux_type_map
+flux_type_map["kustomize"]="kustomization"
+flux_label="toolkit.fluxcd.io"
+
+
+
 # Function to find GitOps owner and Git manifest location
 find_gitops_info() {
     local objectType="$1"
@@ -7,8 +18,49 @@ find_gitops_info() {
     # Replace the following line with the actual command or logic to find the GitOps owner and manifest
     echo "Finding GitOps info for $objectType $objectName..."
     # Dummy values for demonstration
-    echo "Owner: example-owner"
-    echo "Manifest Location: example-location"
+    object_json=$(${KUBERNETES_DISTRIBUTION_BINARY} get "$objectType" "$objectName" -n "$NAMESPACE" --context "$CONTEXT" -o json)
+    
+    flux_match=$(echo "$object_json" | grep "$flux_label")
+    if [[ -n $flux_match ]]; then
+        fetch_flux_owner_details "$object_json"
+    else
+        echo "No label containing '$flux_label' found in $objectType $objectName"
+    fi
+
+    # flux_label_match=$(echo "$object_json" | jq -r ".metadata.labels" | grep )
+    # if [[ -n $flux_label_match ]]; then
+    #     fetch_flux_owner_details(object=$object_json)
+    # else
+    #     echo "Label $flux_label does not exist in the $objectType $objectName"
+    # fi
+}
+
+fetch_flux_owner_details() {
+    local object_json="$1"
+    echo "Processing Flux owner details..."
+    # Get flux type
+    flux_type=$(echo $object_json | jq -r '.metadata.labels | to_entries[] | select(.key | test("^[^.]+\\.toolkit\\.fluxcd\\.io/")) | .key | split(".")[0]' | uniq )
+    flux_type_count=$(echo "$flux_type" | wc -l | awk '{print $1}')
+    if [[ $flux_type_count -gt 1 ]]; then
+        echo "Error: More than one flux type found" >&2
+        exit 1
+    fi
+    flux_name=$(echo "$object_json" | jq -r --arg flux_type "$flux_type" '.metadata.labels[$flux_type + ".toolkit.fluxcd.io/name"]')
+    flux_namespace=$(echo "$object_json" | jq -r --arg flux_type "$flux_type" '.metadata.labels[$flux_type + ".toolkit.fluxcd.io/namespace"]')
+    flux_object_json=$(${KUBERNETES_DISTRIBUTION_BINARY} get ${flux_type_map[$flux_type]} $flux_name -n $NAMESPACE --context $CONTEXT -o json)
+    flux_source=$(echo "$flux_object_json" | jq .spec.sourceRef )
+    flux_source_path=$(echo "$flux_object_json" | jq .spec.path )
+    flux_source_kind=$(echo "$flux_source" | jq .kind )
+    flux_source_name=$(echo "$flux_source" | jq ..name )
+    flux_source_namespace=$(echo "$flux_source" | jq .namespace )
+    flux_source_object=$($(${KUBERNETES_DISTRIBUTION_BINARY} get $flux_source_kind $flux_source_name -n $flux_source_namespace --context $CONTEXT -o json))
+    git_url=$(echo "$flux_source_object" | jq .spec.url )
+    update_github_manifests "$git_url"
+
+}
+
+update_github_manifests () {
+    
 }
 
 # Main script starts here
