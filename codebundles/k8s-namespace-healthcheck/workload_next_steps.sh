93bc33d0bf1425ee8640719c5062a47262c47fe9
Shea Stewart <stewart.shea@gmail.com>
2024-01-30 17:26:15 +0000

    add next step

--- codebundles/k8s-namespace-healthcheck/workload_next_steps.sh
+++ codebundles/k8s-namespace-healthcheck/workload_next_steps.sh
@@ -69,8 +69,12 @@ if [[ $messages =~ "max node group size reached"]]; then
     next_steps+=("Check for Quota Errors")
 fi
 
+if [[ $messages =~ "Health check failed after"]]; then
+    next_steps+=("Check $owner_kind $owner_name Health")
+fi
+
 if [[ -z $next_steps ]]; then
-    next_steps+=("Please review the report logs and escalate the issue if necessary.")
+    next_steps+=("Please review the report output and escalate the issue if necessary.")
 fi
 # Display the list of recommendations
 printf "%s\n" "${next_steps[@]}" | sort | uniq
