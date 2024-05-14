e4995f5fae817a6080f0198165d40efc3e11800b
Jonathan Funk <jonathan.funk@runwhen.com>
2024-05-14 20:56:10 +0000

    Update suspend to remember patches

--- codebundles/k8s-flux-suspend-namespace/suspend_namespace.sh
+++ codebundles/k8s-flux-suspend-namespace/suspend_namespace.sh
@@ -5,16 +5,24 @@
 
 # FLUX_NAMESPACE_LABEL="kustomize.toolkit.fluxcd.io/namespace"
 # FLUX_NAME_LABEL="kustomize.toolkit.fluxcd.io/name"
-FLUX_APPS_NAMESPACE="flux-system"
 
 # Get all deployments, statefulsets, ingress, and services in NAMESPACE with selector FLUX_NAMESPACE_LABEL and FLUX_NAME_LABEL set
 # For each resource, access the flux resource and set spec.suspend to true using FLUX_NAMESPACE_LABEL and FLUX_NAME_LABEL to look it up
 NAMESPACE_WORKLOADS=$(kubectl get all -n $NAMESPACE --selector=kustomize.toolkit.fluxcd.io/name  -oname)
+processed_objects=()
+
 for NAMESPACE_WORKLOAD in $NAMESPACE_WORKLOADS; do
     manifest_json=$(kubectl get $NAMESPACE_WORKLOAD -n chaos-boutique -o json)
     flux_namespace=$(echo "$manifest_json" | jq -r '.metadata.labels["kustomize.toolkit.fluxcd.io/namespace"]')
     flux_object=$(echo "$manifest_json" | jq -r '.metadata.labels["kustomize.toolkit.fluxcd.io/name"]')
-    if [[ "$flux_namespace" == "$FLUX_APPS_NAMESPACE" ]]; then
-        echo "command: kubectl patch $flux_object -n $flux_namespace --type='merge' -p "{\"spec\":{\"suspend\":true}}" "
+
+    # Skip if the flux_object has already been processed
+    if [[ " ${processed_objects[@]} " =~ " ${flux_object} " ]]; then
+        continue
     fi
+
+    kubectl patch $flux_object -n $flux_namespace --type='merge' -p "{\"spec\":{\"suspend\":true}}"
+
+    # Add the flux_object to the processed_objects array
+    processed_objects+=("$flux_object")
 done
