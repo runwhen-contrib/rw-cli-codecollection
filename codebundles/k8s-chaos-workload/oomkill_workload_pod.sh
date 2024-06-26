9f39a903d8235ba813c280f7ce29f2696472f762
Jonathan Funk <jonathan.funk@runwhen.com>
2024-06-26 16:12:44 +0000

    cleanup log messages

--- codebundles/k8s-chaos-workload/oomkill_workload_pod.sh
+++ codebundles/k8s-chaos-workload/oomkill_workload_pod.sh
@@ -12,10 +12,9 @@ pods=$(kubectl get --context "$CONTEXT" pods -n "$NAMESPACE" -l "$selectors" --f
 
 MAX_KILL=1
 MEMORY_PRESSURE_AMOUNT=2147483648 # 2GB
-echo "Starting random pod oomkill in namespace $NAMESPACE"
+echo "Starting $WORKLOAD_NAME random pod oomkill in namespace $NAMESPACE"
 killed_count=0
 for pod_name in $pods; do
-    # Roll a 50/50 chance
     echo "Creating background process to OOMkill pod $pod_name by applying pressure to $MEMORY_PRESSURE_AMOUNT of memory..."
     kubectl exec --context $CONTEXT -n $NAMESPACE $pod_name -- /bin/sh -c 'for i in 1 2 3 4 5; do (while :; do dd if=/dev/zero of=/dev/null bs=2147483648  count=100 & done) & done'
     echo "Checking on pod..."
