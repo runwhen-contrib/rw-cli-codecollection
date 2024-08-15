8a653e2575c947ed10a9d4f307534ef564c7e79b
Jonathan Funk <jonathan.funk@runwhen.com>
2024-08-15 20:51:24 +0000

    Update metric check scripts

--- codebundles/azure-appservice-triage/appservice_health.sh
+++ codebundles/azure-appservice-triage/appservice_health.sh
@@ -3,24 +3,41 @@
 # ENV:
 # AZ_USERNAME
 # AZ_SECRET_VALUE
+# AZ_SUBSCRIPTION
 # AZ_TENANT
 # APPSERVICE
 # AZ_RESOURCE_GROUP
 
+WEBAPP_TYPE="Microsoft.Web/sites"
+HEALTH_METRIC="HealthCheckStatus"
+METRIC_TOP=100
+ALLOWED_MIN=95
+
 # Log in to Azure CLI
-az login --service-principal --username $AZ_USERNAME --password $AZ_SECRET_VALUE --tenant $AZ_TENANT
-
+az login --service-principal --username $AZ_USERNAME --password $AZ_SECRET_VALUE --tenant $AZ_TENANT > /dev/null
 # Set the subscription
-# az account set --subscription $SUBSCRIPTION_ID
+az account set --subscription $AZ_SUBSCRIPTION
+# add the application-insights extension
+# az extension add --name application-insights
+
 # Get the health status of the App Service web app
-health_status=$(az monitor app-insights component show-health --app $APPSERVICE --resource-group $AZ_RESOURCE_GROUP --query 'healthStatus' -o tsv)
-
-# Print the health status
-echo "Health Status: $health_status"
-
-# Check if the health status is not healthy
-if [[ "$health_status" != "Healthy" ]]; then
-    echo "Error: App Service is not healthy"
+health_status=$(az monitor metrics list --resource $APPSERVICE --resource-group $AZ_RESOURCE_GROUP  --resource-type Microsoft.Web/sites --metric "HealthCheckStatus" --interval 5m --aggregation minimum --top $METRIC_TOP)
+healthy=0
+# Loop through the metric time series
+for metric in $(echo "$health_status" | jq -r '.value[].timeseries[].data[].minimum'); do
+    # Check if the metric value is below the allowed min
+    if [[ $metric -lt $ALLOWED_MIN ]]; then
+        healthy=1
+        break
+    fi
+done
+if [ $healthy -eq 1 ]; then
+    echo "Error: The App Service $APPSERVICE has recent health check failures"
+    echo "Health metric timeseries contains values below the allowed minimum: $ALLOWED_MIN"
+    echo ""
+    echo -E "$health_status"
     exit 1
+else
+    echo "The App Service web app is healthy"
+    exit 0
 fi
-exit 0
--- codebundles/azure-appservice-triage/appservice_metrics.sh
+++ codebundles/azure-appservice-triage/appservice_metrics.sh
@@ -8,32 +8,79 @@
 # AZ_RESOURCE_GROUP
 # APPSERVICE
 
-# Log in to Azure CLI
-az login --service-principal --username $AZ_USERNAME --password $AZ_SECRET_VALUE --tenant $AZ_TENANT
+CPU_METRIC="CpuTime"
+MEM_METRIC="AverageMemoryWorkingSet"
+BYTES_IN_METRIC="BytesReceived"
+BYTES_OUT_METRIC="BytesSent"
+ERROR_METRIC="Http5xx"
 
+ALLOWED_ERRORS=0
+ALLOWED_CPU=80
+ALLOWED_MEM=80
+BYTE_THRESHOLD=1
+METRIC_TOP=100
+
+# Log in to Azure CLI
+az login --service-principal --username $AZ_USERNAME --password $AZ_SECRET_VALUE --tenant $AZ_TENANT > /dev/null
 # Set the subscription
 az account set --subscription $AZ_SUBSCRIPTION
 
-# Fetch key metrics for the appservice
-metrics=$(az monitor app-insights metrics show --resource-group $AZ_RESOURCE_GROUP --resource $APPSERVICE --metrics "CpuPercentage MemoryPercentage DiskPercentage")
+ok=0
+next_steps=()
+cpu_data=$(az monitor metrics list --resource $APPSERVICE --resource-group $AZ_RESOURCE_GROUP --resource-type Microsoft.Web/sites --metric "$CPU_METRIC" --interval 5m --aggregation maximum --top $METRIC_TOP)
+mem_data=$(az monitor metrics list --resource $APPSERVICE --resource-group $AZ_RESOURCE_GROUP --resource-type Microsoft.Web/sites --metric "$MEM_METRIC" --interval 5m --aggregation maximum --top $METRIC_TOP)
+bytes_in_data=$(az monitor metrics list --resource $APPSERVICE --resource-group $AZ_RESOURCE_GROUP --resource-type Microsoft.Web/sites --metric "$BYTES_IN_METRIC" --interval 5m --aggregation average --top $METRIC_TOP)
+bytes_out_data=$(az monitor metrics list --resource $APPSERVICE --resource-group $AZ_RESOURCE_GROUP --resource-type Microsoft.Web/sites --metric "$BYTES_OUT_METRIC" --interval 5m --aggregation average --top $METRIC_TOP)
+error_data=$(az monitor metrics list --resource $APPSERVICE --resource-group $AZ_RESOURCE_GROUP --resource-type Microsoft.Web/sites --metric "$ERROR_METRIC" --interval 5m --aggregation maximum --top $METRIC_TOP)
 
-# Check if any metrics are overutilized/unhealthy
-overutilized=0
+for metric in $(echo "$cpu_data" | jq -r '.value[].timeseries[].data[].maximum | numbers'); do
+    if (( $(echo "$metric > $ALLOWED_CPU" | bc -l) )); then
+        echo "Found CPU usage above the allowed threshold: $ALLOWED_CPU for Azure App Service $APPSERVICE in $AZ_RESOURCE_GROUP"
+        next_steps+=("Increase Azure App Service $APPSERVICE CPU\n")
+        ok=1
+        break
+    fi
+done
+for metric in $(echo "$mem_data" | jq -r '.value[].timeseries[].data[].maximum'); do
+    if [[ $metric -gt $ALLOWED_MEM ]]; then
+        echo "Found Memory usage above the allowed threshold: $ALLOWED_MEM for Azure App Service $APPSERVICE in $AZ_RESOURCE_GROUP"
+        next_steps+=("Increase Azure App Service $APPSERVICE memory\n")
+        ok=1
+        break
+    fi
+done
+for metric in $(echo "$bytes_in_data" | jq -r '.value[].timeseries[].data[].average'); do
+    if [[ $metric -lt $BYTE_THRESHOLD ]]; then
+        echo "App service $APPSERVICE has low or no bytes received, where threshold is set as: $BYTE_THRESHOLD"
+        next_steps+=("Verify that Azure App Service $APPSERVICE in $AZ_RESOURCE_GROUP healthy and that the network is configured correctly\n")
+        ok=1
+        break
+    fi
+done
+for metric in $(echo "$bytes_out_data" | jq -r '.value[].timeseries[].data[].average'); do
+    if [[ $metric -lt $BYTE_THRESHOLD ]]; then
+        echo "App service $APPSERVICE has low or no bytes sent, where threshold is set as: $BYTE_THRESHOLD"
+        next_steps+=("Verify that Azure App Service $APPSERVICE in $AZ_RESOURCE_GROUP healthy and that the network is configured correctly\n")
+        ok=1
+        break
+    fi
+done
+for metric in $(echo "$error_data" | jq -r '.value[].timeseries[].data[].maximum'); do
+    if [[ $metric -gt $ALLOWED_ERRORS ]]; then
+        echo "Found HTTP 5xx errors above the allowed threshold: $ALLOWED_ERRORS for $APPSERVICE"
+        next_steps+=("Investigate the HTTP 5xx errors for Azure App Service $APPSERVICE in $AZ_RESOURCE_GROUP\n")
+        ok=1
+        break
+    fi
+done
 
-cpu_percentage=$(echo $metrics | jq -r '.value[0].timeseries[0].data[0].average')
-if (( $(echo "$cpu_percentage > 80" | bc -l) )); then
-    overutilized=1
+if [ $ok -eq 1 ]; then
+    echo "Error: The Azure App Service $APPSERVICE failed one or more key metric checks"
+    echo ""
+    echo "Next Steps:"
+    echo -e "${next_steps[@]}"
+    exit 1
+else
+    echo "Azure App Service $APPSERVICE in $AZ_RESOURCE_GROUP passed all key metric checks"
+    exit 0
 fi
-
-memory_percentage=$(echo $metrics | jq -r '.value[1].timeseries[0].data[0].average')
-if (( $(echo "$memory_percentage > 80" | bc -l) )); then
-    overutilized=1
-fi
-
-disk_percentage=$(echo $metrics | jq -r '.value[2].timeseries[0].data[0].average')
-if (( $(echo "$disk_percentage > 80" | bc -l) )); then
-    overutilized=1
-fi
-
-# Return 1 if any metrics are overutilized/unhealthy
-exit $overutilized
