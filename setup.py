0f0a94d984ec1740e694f8f1e6f7998ce2d9164d
Shea Stewart <stewart.shea@gmail.com>
2024-11-04 22:30:59 -0500

    update pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 setup(
-    name="runwhen-cli-keywords",
+    name="rw-cli-keywords",
     version=open("VERSION").read(),
     packages=["RW"],
     package_dir={"RW": "RW"},
@@ -12,8 +12,8 @@ setup(
     description="A set of RunWhen published CLI keywords and python libraries for interacting with APIs using CLIs",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Kyle Forster",
-    author_email="kyle.forster@runwhen.com",
+    author="RunWhen",
+    author_email="info@runwhen.com",
     url="https://github.com/runwhen-contrib/rw-cli-codecollection",
     install_requires=required,
     include_package_data=True,
