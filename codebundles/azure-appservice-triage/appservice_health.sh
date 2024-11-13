c2f265884c7851e7be45857ce44f91cc887971a3
Shea Stewart <stewart.shea@gmail.com>
2024-11-13 14:33:53 +0000

    remove one codebundle. tune up the rest to support per resource subscriptino ids

--- codebundles/azure-appservice-triage/appservice_health.sh
+++ codebundles/azure-appservice-triage/appservice_health.sh
@@ -8,15 +8,24 @@
 # APPSERVICE
 # AZ_RESOURCE_GROUP
 
+# Check if AZURE_RESOURCE_SUBSCRIPTION_ID is set, otherwise get the current subscription ID
+if [ -z "$AZURE_RESOURCE_SUBSCRIPTION_ID" ]; then
+    subscription=$(az account show --query "id" -o tsv)
+    echo "AZURE_RESOURCE_SUBSCRIPTION_ID is not set. Using current subscription ID: $subscription"
+else
+    subscription="$AZURE_RESOURCE_SUBSCRIPTION_ID"
+    echo "Using specified subscription ID: $subscription"
+fi
+
+# Set the subscription to the determined ID
+echo "Switching to subscription ID: $subscription"
+az account set --subscription "$subscription" || { echo "Failed to set subscription."; exit 1; }
+
+
 WEBAPP_TYPE="Microsoft.Web/sites"
 HEALTH_METRIC="HealthCheckStatus"
 METRIC_TOP=100
 ALLOWED_MIN=95
-
-# # Log in to Azure CLI
-# az login --service-principal --username $AZ_USERNAME --password $AZ_SECRET_VALUE --tenant $AZ_TENANT > /dev/null
-# # Set the subscription
-# az account set --subscription $AZ_SUBSCRIPTION
 
 # Get the health status of the App Service web app
 health_status=$(az monitor metrics list --resource $APPSERVICE --resource-group $AZ_RESOURCE_GROUP  --resource-type Microsoft.Web/sites --metric "HealthCheckStatus" --interval 5m --aggregation minimum --top $METRIC_TOP)
