72e17a072a6114ab91f6dbf298a2deb818930749
Shea Stewart <stewart.shea@gmail.com>
2026-02-14 06:35:37 +0000

    Refactor AWS authentication handling in multiple scripts
    - Unified the authentication process by sourcing a common `auth.sh` script across various AWS-related scripts, enhancing maintainability and reducing code duplication.
    - Updated the `auth.sh` script to improve the handling of AWS credentials, including verification of identity and role assumption logic.
    - Improved documentation within the scripts to clarify usage and expected environment setup for AWS credentials.

--- codebundles/aws-eks-health/check_eks_fargate_cluster_health_status.sh
+++ codebundles/aws-eks-health/check_eks_fargate_cluster_health_status.sh
@@ -2,23 +2,7 @@
 
 # Environment Variables:
 # AWS_REGION
-auth() {
-    # if required AWS_ cli vars are not set, error and exit 1
-    if [[ -z $AWS_ACCESS_KEY_ID || -z $AWS_SECRET_ACCESS_KEY  || -z $AWS_REGION ]]; then
-        echo "AWS credentials not set. Please set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables."
-        exit 1
-    fi
-    # if AWS_ROLE_ARN then assume the role using sts and override the pre-existing key ENVs
-    if [[ -n $AWS_ROLE_ARN ]]; then
-        sts_output=$(aws sts assume-role --role-arn "$AWS_ROLE_ARN" --role-session-name "AssumeRoleSession")
-        AWS_ACCESS_KEY_ID=$(echo "$sts_output" | jq -r '.Credentials.AccessKeyId')
-        AWS_SECRET_ACCESS_KEY=$(echo "$sts_output" | jq -r '.Credentials.SecretAccessKey')
-        AWS_SESSION_TOKEN=$(echo "$sts_output" | jq -r '.Credentials.SessionToken')
-        export AWS_ACCESS_KEY_ID
-        export AWS_SECRET_ACCESS_KEY
-        export AWS_SESSION_TOKEN
-    fi
-}
+source "$(dirname "$0")/auth.sh"
 auth
 
 # get list of eks clusters
